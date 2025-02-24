a1f4da37d95a02d0ff92a51c19d0bdf5a3a30908
anisa <anisahime@gmail.com>
2025-02-24 23:18:38 +1000

    add and setting urls, views in catalog

---
+++ catalog/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    """Контроллер для домашней страницы"""
+    return render(request, "home.html")
+
+
+def contacts(request):
+    """Контроллер для страницы контактов"""
+    if request.method == "POST":
+        name = request.POST.get("name")
+        message = request.POST.get("message")
+        return HttpResponse(
+            f"Спасибо, {name}! Ваше сообщение принято и будет рассмотрено в ближайшее время."
+        )
+    return render(request, "contacts.html")
