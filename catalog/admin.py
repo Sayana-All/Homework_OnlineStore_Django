0d0f8c42464dfedae2a7d2e5c685e7cd0bf0b7c6
anisa <anisahime@gmail.com>
2025-03-02 04:20:58 +1000

    file correction

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,17 +1,20 @@
 from django.contrib import admin
+
 from catalog.models import Category, Product
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     """Класс категорий для админки"""
+
     list_display = ("id", "name")
     search_fields = ("name", "description")
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    """Класс категорий для админки"""
+    """Класс продуктов для админки"""
+
     list_display = ("id", "name", "price", "category")
     list_filter = ("category",)
     search_fields = ("name", "description", "category__name")
