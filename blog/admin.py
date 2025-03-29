278fb7f2f66df17642adac4ac3fce2d4b7cf4fee
anisa <anisahime@gmail.com>
2025-03-29 10:19:45 +1000

    add models and blog templates base + includes

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import BlogArticle
+
+@admin.register(BlogArticle)
+class AdminBlogArticle(admin.ModelAdmin):
+    """Класс статей для админки"""
+
+    list_display = ("id", "title", "created_at", "is_publication", "views_counter")
+    list_filter = ("title", "created_at", "views_counter",)
+    search_fields = ("id", "title", "created_at")
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class BlogArticle(models.Model):
+    """Модель блоговой записи"""
+    title = models.CharField(max_length=100, verbose_name="Заголовок", help_text="Введите заголовок записи")
+    content = models.TextField(verbose_name="Содержимое", help_text="Введите текст записи")
+    preview = models.ImageField(
+        upload_to="preview/", blank=True, null=True, verbose_name="Изображение", help_text="Загрузите изображение для превью"
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    is_publication = models.BooleanField(default=True, verbose_name="Признак публикации")
+    views_counter = models.PositiveIntegerField(verbose_name="Количество просмотров", help_text="Укажите количество просмотров", default=0)
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = "статья"
+        verbose_name_plural = "статьи"
+        ordering = [
+            "title", "created_at", "views_counter",
+        ]
