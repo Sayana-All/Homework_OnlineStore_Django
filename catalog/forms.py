702d803763f9497e263dd4688e09ec0efd15e859
anisa <anisahime@gmail.com>
2025-04-04 01:08:40 +1000

    add forms.py and forbidden_words

---
+++ catalog/forms.py
@@ -0,0 +1,65 @@
+from django.core.exceptions import ValidationError
+from django.forms import ModelForm, BooleanField
+
+from catalog.models import Product, Category
+from config.settings import forbidden_words
+
+
+class StyleFormMixin:
+    """Класс для стилизации форм"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs["class"] = "form-check-input"
+            else:
+                field.widget.attrs["class"] = "form-control"
+
+
+class ProductForm(ModelForm):
+    """Класс формы для создания и редактирования модели продуктов"""
+
+    class Meta:
+        model = Product
+        exclude = ["created_at", "updated_at"]
+
+    def __init__(self, *args, **kwargs):
+        super(ProductForm, self).__init__(*args, **kwargs)
+        self.fields["name"].widget.attrs.update(
+            {"class": "form-control", "placeholder": "Введите наименование товара"}
+        )
+        self.fields["description"].widget.attrs.update(
+            {"class": "form-control", "placeholder": "Введите описание товара"}
+        )
+        self.fields["photo"].widget.attrs.update({"class": "form-input"})
+        self.fields["category"].widget.attrs.update({"class": "form-check"})
+        self.fields["price"].widget.attrs.update({"class": "form-control", "placeholder": "Введите цену"})
+
+    def clean_price(self):
+        """Метод для проверки цены на отрицательные значения"""
+        price = self.cleaned_data.get("price")
+        if price < 0:
+            raise ValidationError("Цена не должна иметь отрицательное значение!")
+        return price
+
+    def clean(self):
+        """Метод для проверки имени и описания на запрещенные слова"""
+        cleaned_data = super().clean()
+        name = cleaned_data.get("name")
+        description = cleaned_data.get("description")
+
+        for word in forbidden_words:
+            if word in name.lower():
+                self.add_error("name", "Внимание! Не используйте запрещенные слова в наименовании продукта")
+
+            elif word in description.lower():
+                self.add_error("description", "Внимание! Не используйте запрещенные слова в описании продукта")
+
+
+class CategoryForm(StyleFormMixin, ModelForm):
+    """Класс формы для создания и редактирования модели категорий"""
+
+    class Meta:
+        model = Category
+        fields = "__all__"
--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,17 @@
+from django.forms import inlineformset_factory
 from django.http import HttpResponse
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, TemplateView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
+from catalog.forms import ProductForm
 from catalog.models import Product
+from config.settings import forbidden_words
 
 
 class ProductListView(ListView):
     """Контроллер со списком продуктов для домашней страницы"""
+
     model = Product
     template_name = "catalog/product_list.html"
     context_object_name = "products"
@@ -15,6 +19,7 @@ class ProductListView(ListView):
 
 class ProductDetailView(DetailView):
     """Контроллер для отображения детальной информации о продукте"""
+
     model = Product
     template_name = "catalog/product_detail.html"
     context_object_name = "product"
@@ -22,22 +27,65 @@ class ProductDetailView(DetailView):
 
 class ProductCreateView(CreateView):
     """Контроллер для добавления нового продукта"""
+
     model = Product
-    fields = ["name", "description", "photo", "category", "price"]
+    form_class = ProductForm
     template_name = "catalog/product_form.html"
     success_url = reverse_lazy("catalog:product_list")
+
+    def get_context_data(self, **kwargs):
+        """Переопределяем метод для добавления списка запрещенных слов в форму"""
+        context = super().get_context_data(**kwargs)
+        context["forbidden_words"] = forbidden_words
+        return context
 
 
 class ProductUpdateView(UpdateView):
     """Контроллер для редактирования существующего продукта"""
+
     model = Product
-    fields = ["name", "description", "photo", "category", "price"]
+    form_class = ProductForm
     template_name = "catalog/product_form.html"
     success_url = reverse_lazy("catalog:product_list")
+
+    def get_success_url(self):
+        """Метод для изменения адреса перенаправления после редактирования записи"""
+        return reverse("catalog:product_detail", args=[self.kwargs.get("pk")])
+
+    def get_context_data(self, **kwargs):
+        """Переопределяем метод для добавления списка запрещенных слов в форму"""
+        context = super().get_context_data(**kwargs)
+        context["forbidden_words"] = forbidden_words
+        return context
+
+    # def get_context_data(self, **kwargs):
+    #    """Метод для изменения и добавления новой категории товара"""
+    #    context_data = super().get_context_data(**kwargs)
+    #    ProductFormset = inlineformset_factory(Product, Category, CategoryForm, extra=1)
+    #    if self.request.method == "POST":
+    #        context_data["formset"] = ProductFormset(self.request.POST, instance=self.object)
+    #    else:
+    #        context_data["formset"] = ProductFormset(instance=self.object)
+    #    return context_data
+
+
+#
+# def form_valid(self, form):
+#    """Метод для переопределения валидации формы"""
+#    context_data = self.get_context_data()
+#    formset = context_data["formset"]
+#    if form.is_valid() and formset.is_valid():
+#        self.object = form.save()
+#        formset.instance = self.object
+#        formset.save()
+#        return super().form_valid(form)
+#    else:
+#        return self.render_to_response(self.get_context_data(form=form, formset=formset))
 
 
 class ProductDeleteView(DeleteView):
     """Контроллер для удаления продукта"""
+
     model = Product
     template_name = "catalog/product_confirm_delete.html"
     success_url = reverse_lazy("catalog:product_list")
@@ -45,6 +93,7 @@ class ProductDeleteView(DeleteView):
 
 class ContactsTemplateView(TemplateView):
     """Контроллер для страницы контактов"""
+
     template_name = "catalog/contacts.html"
 
     def post(self, request, *args, **kwargs):
