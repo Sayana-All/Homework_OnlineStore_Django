71320b3d867b127d2bc4189ff2e7ce8c507a0bfb
anisa <anisahime@gmail.com>
2025-04-13 07:04:57 +1000

    update for navbar

--- config/settings.py
+++ config/settings.py
@@ -53,6 +53,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "catalog.context_processors.categories_processor",
             ],
         },
     },
@@ -134,8 +135,8 @@ CACHE_ENABLED = True
 
 if CACHE_ENABLED:
     CACHES = {
-        'default': {
-            'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-            'LOCATION': 'redis://127.0.0.1:6379/1',
+        "default": {
+            "BACKEND": "django.core.cache.backends.redis.RedisCache",
+            "LOCATION": os.getenv("LOCATION"),
         }
     }
